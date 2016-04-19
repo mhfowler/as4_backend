5ac2b22bfbefc0ae80548191ca3e3190340248e1
Max Fowler <maxhfowler@gmail.com>
2016-04-19 00:29:09 -0400

    stripped title

--- hello_webapp/app.py
+++ hello_webapp/app.py
@@ -45,7 +45,8 @@ def save_note_endpoint():
     text = request.form['text']
     lines = text.split('\n')
     title = lines[0]
-    save_note_to_dropbox(title=title, text=text)
+    stripped_title = re.sub(r'\W+', '', title)
+    save_note_to_dropbox(title=stripped_title, text=text)
     _log('++ saved note: '.format(text))
     return 'saved'
 
