dbba8ad48aa7508ff910b7e51beb1918b37fd59c
Max Fowler <maxhfowler@gmail.com>
2016-04-25 18:11:06 -0400

    process note, re-arrange lines

--- hello_utilities/process_note.py
+++ hello_utilities/process_note.py
@@ -12,14 +12,22 @@ def get_hashtags(text):
 
 
 def get_commands(text):
-    hashtags = {tag.strip("/").encode('ascii', 'ignore') for tag in text.split() if tag.startswith("/")}
+    hashtags = {tag.strip("\\").encode('ascii', 'ignore') for tag in text.split() if tag.startswith("\\")}
     return hashtags
 
 
 def strip_data(text):
     lines = text.split('\n')
-    note_lines = filter(lambda line: not (line.startswith('url: ') or line.startswith('img: ')), lines)
+    note_lines = filter(lambda line: not (line.startswith('source: ') or line.startswith('img: ')), lines)
+    note_lines = [strip_line(line) for line in note_lines]
     stripped = '\n'.join(note_lines)
+    return stripped
+
+
+def strip_line(line):
+    words = line.split(' ')
+    words = filter(lambda word: not word.startswith('#') and not word.startswith('\\'), words)
+    stripped = ' '.join(words)
     return stripped
 
 
@@ -38,20 +46,31 @@ def process_note(text, title):
     if 'meme' in commands:
         tumblr_post_img(text)
     # save to evernote
-    hashtags = get_hashtags(text)
+    hashtags = list(get_hashtags(text))
     _log('++ found hashtags: {}'.format(str(hashtags)), debug=True)
-    tags = get_hashtags(text)
-    if not tags:
+    if not hashtags:
         notebook_name = 'as4'
     else:
-        notebook_name = tags.pop()
+        notebook_name = hashtags[0]
     # save a simplenote
-    tags.add('as4notes')
-    save_simplenote(note_text=text, tags=list(tags))
+    # remove hashtags from note and put them at the bottom
+    lines = text.split('\n')
+    lines = [strip_line(line) for line in lines]
+    simplenote_text = '\n'.join(lines)
+    # add metadata to the bottom
+    hashtag_line = ' '.join([('#' + tag) for tag in hashtags])
+    simplenote_text += '\n' + hashtag_line
+    hashtags.append('as4notes')
+    save_simplenote(note_text=simplenote_text, tags=hashtags)
     # save an evernote
     save_evernote(note_title=title, note_text=text, notebook_name=notebook_name)
 
 
 if __name__ == '__main__':
-    process_note('hello how are you?\n img: https://41.media.tumblr.com/a02410dc6a319722b3c77207e7a3b039/tumblr_o5uc36xK271qcphy8o1_1280.jpg\n'
-                 '\n\n #test #banana', title='test as4')
+
+    note_text = 'source: https://www.tumblr.com/dashboard\n\n' \
+                'img: https://40.media.tumblr.com/tumblr_luyygcHHPZ1qh2axio1_1280.jpg\n\n' \
+                'stop the war \meme '
+
+    process_note(text=note_text,
+                 title='test as4')
