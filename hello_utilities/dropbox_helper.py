2dd48701b193b62da3177fca449e8a2d6aff5868
Max Fowler <maxhfowler@gmail.com>
2016-04-25 00:17:21 -0400

    fetch screenshots

--- hello_utilities/dropbox_helper.py
+++ hello_utilities/dropbox_helper.py
@@ -1,4 +1,5 @@
 import tempfile
+import re
 import os
 import random
 
@@ -24,5 +25,38 @@ def save_note_to_dropbox(title, text):
     os.system('rm {}'.format(temp_path))
 
 
+def download_dropbox_file(dropbox_path, local_path):
+    f, metadata = client.get_file_and_metadata(dropbox_path)
+    with open(local_path, 'wb') as out_file:
+        out_file.write(f.read())
+
+
+def get_folder_contents(dropbox_path):
+    folder_metadata = client.metadata(dropbox_path)
+    print "metadata:", folder_metadata
+
+
+def fetch_screenshot(screenshot_url):
+    # TODO: replace with tempfile
+    title = str(random.randint(0, 10000000))
+    temp_path = os.path.join(TEMP_PATH, str(title))
+    # download the dropbox file to temp_path using API
+    screenshot_match = re.search('(Screenshot.*\.png)', screenshot_url)
+    file_name = screenshot_match.group(1)
+    dropbox_path = file_name.replace('%20', ' ')
+    dropbox_path = '/Screenshots/' + dropbox_path
+    download_dropbox_file(dropbox_path=dropbox_path, local_path=temp_path)
+    return temp_path
+
+
 if __name__ == '__main__':
-    save_note_to_dropbox(title='test', text='test note test note')
+    # save_note_to_dropbox(title='test', text='test note test note')
+    temp_path = os.path.join(TEMP_PATH, 'dropboxtest.png')
+    d_path = '/Screenshots/Screenshot 2016-04-24 23.53.41.png'
+    # d_path = '/s/h0nty3y9633jzql/Screenshot%202016-04-24%2023.53.41.png'
+    # d_path = '/desktop/art/carpet.jpg'
+    download_dropbox_file(dropbox_path=d_path, local_path=temp_path)
+
+    # get folder contents
+    # d_path = '/Screenshots'
+    # get_folder_contents(dropbox_path=d_path)
--- hello_utilities/tumblr_helper.py
+++ hello_utilities/tumblr_helper.py
@@ -1,7 +1,11 @@
 import pytumblr
+import random
+import os
+import re
 
-from hello_settings import SECRETS_DICT
+from hello_settings import SECRETS_DICT, TEMP_PATH
 from hello_utilities.log_helper import _log
+from hello_utilities.dropbox_helper import fetch_screenshot
 
 
 client = pytumblr.TumblrRestClient(
@@ -14,7 +18,15 @@ client = pytumblr.TumblrRestClient(
 
 def post_photo_to_tumblr(photo_url, caption, tumblr='memecollections.tumblr.com'):
     photo_url = photo_url.strip()
-    created = client.create_photo(tumblr, caption=caption, state="published", source=photo_url)
+    # if a dropbox image, fetch it to a temp file, and then post it
+    if photo_url.startswith('https://www.dropbox.com/s/'):
+        temp_path = fetch_screenshot(screenshot_url=photo_url)
+        created = client.create_photo(tumblr, caption=caption, state="published", data=[temp_path])
+        os.system('rm {}'.format(temp_path))
+    # if a public image
+    else:
+        created = client.create_photo(tumblr, caption=caption, state="published", source=photo_url)
+    # log the new post
     post_url = 'http://{tumblr}/image/{id}'.format(
         tumblr=tumblr,
         id=created['id']
@@ -23,6 +35,7 @@ def post_photo_to_tumblr(photo_url, caption, tumblr='memecollections.tumblr.com'
 
 
 if __name__ == '__main__':
-    photo = 'https://41.media.tumblr.com/033d1acfdc5e35c7c5b69591506903b6/tumblr_o5ycowfwXs1rdqms8o1_1280.jpg'
-    caption = 'waves'
+    # photo = 'https://41.media.tumblr.com/033d1acfdc5e35c7c5b69591506903b6/tumblr_o5ycowfwXs1rdqms8o1_1280.jpg'
+    photo = 'https://www.dropbox.com/s/h0nty3y9633jzql/Screenshot%202016-04-24%2023.53.41.png?dl=0'
+    caption = 'screenshot test2'
     post_photo_to_tumblr(photo_url=photo, caption=caption)
